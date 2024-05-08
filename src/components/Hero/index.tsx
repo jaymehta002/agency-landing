11c56ed576af51cae87505f12e4fdccd3d0abc31
jaymehta002 <jaymehta002@gmail.com>
2024-05-08 12:37:23 +0530

    initial

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+const Hero = () => {
+  return (
+    <>
+    <h1>heading</h1>
+    diva
+    <div> sad-p</div>
+    </>
+  )
+}
+
+export default Hero
