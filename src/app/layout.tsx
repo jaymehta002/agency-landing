5b3f39acd2c0003e039bf734cc02241e0c64f5ce
jaymehta002 <jaymehta002@gmail.com>
2024-05-25 13:19:26 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
